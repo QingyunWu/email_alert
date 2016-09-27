168d5291e10905619d5bfb5a45e7fbaa60f897e8
QingyunWu <qingyun.wu559@gmail.comfds>
2016-09-26 21:52:58 -0400

    first commit

--- src/config.py
+++ src/config.py
@@ -2,7 +2,6 @@ __author__ = 'Qingyun Wu'
 
 DEBUG = True
 ADMINS = frozenset([
-	"qingyun.wu559@gmail.com",
 	"roger_5@qq.com",
 	"qw559@nyu.edu"
 ])
--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,15 @@
+import os
 __author__ = 'Qingyun Wu'
 
+# hide key in environment variables
 
-URL = "https://api.mailgun.net/v3/sandbox99ea708e421348deae802e401298d8ea.mailgun.org/messages"
-API_KEY = "key-f09f492eaede684f145a2fda9e151f6f"
-FROM = "postmaster@sandbox99ea708e421348deae802e401298d8ea.mailgun.org"
+# URL = "https://api.mailgun.net/v3/sandbox99ea708e421348deae802e401298d8ea.mailgun.org/messages"
+URL = os.environ.get('MAILGUN_URL')
+# API_KEY = "key-f09f492eaede684f145a2fda9e151f6f"
+API_KEY = os.environ.get('MAILGUN_KEY')
+
+# FROM = "postmaster@sandbox99ea708e421348deae802e401298d8ea.mailgun.org"
+FROM = os.environ.get('MAILGUN_FROM')
+
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
