ff05397c8fb293672e6411ab789dc788a10163dd
QingyunWu <qingyun.wu559@gmail.comfds>
2016-09-26 22:28:53 -0400

    mongoldb

--- src/common/database.py
+++ src/common/database.py
@@ -1,10 +1,11 @@
 import pymongo
-
+import os
 __author__ = 'Qingyun Wu'
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
+    # use environment variables in heroku to hide username and pw
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
 
     @staticmethod
